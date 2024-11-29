6592380e82bffdc1b9bc63871be275c468c95764
srm <saadrm.dev@gmail.com>
2024-11-29 22:41:41 +0100

    refactor: ValidateArtistID()

--- app/utils/input_validator.go
+++ app/utils/input_validator.go
@@ -6,28 +6,25 @@ import (
 )
 
 func ValidateArtistID(queries map[string][]string) (string, int) {
-	msg := "ok"
-	status := http.StatusOK
-
 	if queries["id"] == nil {
 		return "400 - missing id", http.StatusBadRequest
 	}
 
 	artistIDStr := queries["id"][0]
 
+	if artistIDStr == "" {
+		return "400 - empty artist id", http.StatusBadRequest
+	}
+
 	artistID, err := strconv.Atoi(artistIDStr)
 
-	if artistID < 1 || artistID > 52 {
-		msg, status = "404 - artist not found", http.StatusNotFound
-	}
-
 	if err != nil {
-		msg, status = "400 - invalid artist id", http.StatusBadRequest
+		return "400 - invalid artist id", http.StatusBadRequest
 	}
 
-	if artistIDStr == "" {
-		msg, status = "400 - invalid empty artist id", http.StatusBadRequest
+	if artistID < 1 || artistID > 52 {
+		return "404 - artist not found", http.StatusNotFound
 	}
 
-	return msg, status
+	return "ok", http.StatusOK
 }
